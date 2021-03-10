b82709aa567920b289eb794f29f5d147376448cc
brueck1988 <brueck1988@gmail.com>
2021-03-10 09:41:32 -0700

    Updated stock test in vender test

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -20,7 +20,7 @@ class VendorTest < Minitest::Test
 
   def test_check_stock
     vendor = Vendor.new("Rocky Mountain Fresh")
-
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
     assert_equal 0, vendor.check_stock(item1)
   end
 
@@ -28,8 +28,14 @@ class VendorTest < Minitest::Test
     vendor = Vendor.new("Rocky Mountain Fresh")
     item1 = Item.new({name: 'Peach', price: "$0.75"})
     item2 = Item.new({name: 'Tomato', price: '$0.50'})
-
+    vendor.stock(item1, 30)
     expected = {item1 => 30}
-    assert_equal expected, vendor.stock(item1, 30)
+    assert_equal expected, vendor.inventory
+    assert_equal 30, vendor.check_stock(item1)
+    vendor.stock(item1, 25)
+    assert_equal 55, vendor.check_stock(item1)
+    vendor.stock(item2, 12)
+    expected =  {item1 => 55, item2 => 12}
+    assert_equal expected, vendor.inventory
   end
 end
