9c3544369af659e3f8d862cd315aa128a06a7c9f
brueck1988 <brueck1988@gmail.com>
2021-03-10 09:11:08 -0700

    Created tests for item and vendor classes

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -4,4 +4,23 @@ require './lib/item'
 require './lib/vendor'
 
 class VendorTest < Minitest::Test
+  def test_it_exists
+    vendor = Vendor.new("Rocky Mountain Fresh")
+
+    assert_instance_of Vendor, vendor
+  end
+
+  def test_it_has_attributes
+    vendor = Vendor.new("Rocky Mountain Fresh")
+
+    assert_equal "Rocky Mountain Fresh", vendor.name
+    expected = {}
+    assert_equal expected, vendor.inventory
+  end
+
+  # def test_stock
+  #   vendor = Vendor.new("Rocky Mountain Fresh")
+  #
+  #   assert_equal expected, vendor.inventory
+  # end
 end
